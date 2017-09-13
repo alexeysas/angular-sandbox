8897acf2ff056f6493afaba7cdd680612380174c
Alexey Sas <asasryl@gmail.com>
2017-09-13 18:20:55 +0300

    some stuff

--- sample-spa/src/app/app.module.ts
+++ sample-spa/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { MaterialModule } from '@angular/material'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import 'hammerjs';
-import { MenuComponent } from './menu/menu.component'
+import { MenuComponent } from './menu/menu.component';
+import { MenuV2Component } from './menu-v2/menu-v2.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    MenuV2Component
   ],
   imports: [
     BrowserModule,
