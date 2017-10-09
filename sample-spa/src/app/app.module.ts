83cd06327c859ef970d97ffb01f249fa0b45ae02
Alexey Sas <asasryl@gmail.com>
2017-10-09 15:18:06 +0300

    validation

--- sample-spa/src/app/app.module.ts
+++ sample-spa/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; 
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { MaterialModule } from '@angular/material'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import 'hammerjs';
+
+import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component'
 import { HeaderComponent } from './header/header.component';
@@ -18,7 +20,8 @@ import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
-import { AppRoutingModule } from './app-routing/app-routing.module'
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
   declarations: [
@@ -29,19 +32,24 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     DishService,
     PromotionService,
     LeaderService
+  ],
+  entryComponents: [
+    LoginComponent
   ],
   bootstrap: [AppComponent]
 })
---
+++ sample-spa/src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import {MdDialog, MdDialogRef } from '@angular/material' 
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private dialogRef: MdDialogRef<LoginComponent>) { }
+
+  user = {
+    remember: false
+  };
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log("User: ", this.user);
+    this.dialogRef.close();
+  }
+
+}
